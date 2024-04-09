4d9431a81fcfc781384e009b5926dd25f7c225de
Febriyadi <flutterlearning6@gmail.com>
2024-04-10 06:39:47 +0800

    add ListView code

--- Modul 2/app/src/main/java/com/pebdev/listview/MainActivity.java
+++ Modul 2/app/src/main/java/com/pebdev/listview/MainActivity.java
@@ -2,13 +2,66 @@ package com.pebdev.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.pebdev.listview.adapter.HeroAdapter;
+import com.pebdev.listview.detail.Detail_Activity;
+import com.pebdev.listview.entity.Hero;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class    MainActivity extends AppCompatActivity {
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private HeroAdapter adapter;
+    private ArrayList<Hero> heroes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adapter = new HeroAdapter(this);
+        ListView listView = findViewById(R.id.lv_list);
+        List<String> list = new ArrayList<>();
+        list.add("Yunita_070");
+        ArrayAdapter arrayAdapter =  new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                if (position==0){
+                    startActivity(new Intent(MainActivity.this, Detail_Activity.class));
+                }
+            }
+        });
+        listView.setAdapter(adapter);
+        prepare();
+        addItem();
+    }
+
+    private void addItem() {
+        heroes = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            Hero hero = new Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+        adapter.setHeroes(heroes);
+    }
+
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
     }
 }
---
+++ Modul 2/app/src/main/java/com/pebdev/listview/detail/Detail_Activity.java
@@ -0,0 +1,16 @@
+package com.pebdev.listview.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pebdev.listview.R;
+
+public class Detail_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
