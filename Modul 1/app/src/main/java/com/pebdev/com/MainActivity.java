fe4217ce354efb8d508ba1b98154cf601a4546ec
Febriyadi <flutterlearning6@gmail.com>
2024-04-09 17:26:54 +0800

    add volume Bar

--- Modul 1/app/src/main/java/com/pebdev/com/MainActivity.java
+++ Modul 1/app/src/main/java/com/pebdev/com/MainActivity.java
@@ -3,12 +3,76 @@ package com.pebdev.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edtWidth;
+    private EditText edtHeight;
+    private EditText edtLength;
+    private Button btnCalculate;
+    private TextView tv_result;
+
+    private static final String STATE_RESULT = "state_result";
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_RESULT, tv_result.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_length);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tv_result = findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
+
+        if (savedInstanceState != null) {
+            String result = savedInstanceState.getString(STATE_RESULT);
+            tv_result.setText(result);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_calculate) {
+            String inputLength = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
+
+
+            boolean isEmptyFields = false;
+
+            if (TextUtils.isEmpty(inputLength)) {
+                isEmptyFields = true;
+                edtLength.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputWidth)) {
+                isEmptyFields = true;
+                edtWidth.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputHeight)) {
+                isEmptyFields = true;
+                edtHeight.setError("Field ini tidak boleh kosong");
+            }
+
+            if (!isEmptyFields) {
+                Double volume = Double.parseDouble(inputLength) * Double.parseDouble(inputWidth) *
+                        Double.parseDouble(inputHeight);
+                tv_result.setText(String.valueOf(volume));
+            }
+        }
     }
 }
